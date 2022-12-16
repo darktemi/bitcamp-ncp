62a064aa6e771f6ec75a45bf2b93a19de4a46000
darktemi <darktemi90@nate.com>
2022-12-16 11:37:15 +0900

    class

--- javascript/ex07/app.js
+++ javascript/ex07/app.js
@@ -1,5 +1,9 @@
 // express 라이브러리 로딩하기
+const { response } = require('express');
 const express = require('express');
+// HTTP 요청을 다루는 라이브러리 로딩하기
+const request = require('request');
+
 const port = 3000; // 웹 서버 포트 번호
 
 // express()를 호출하여 웹 서버를 준비
@@ -11,9 +15,24 @@ app.get(                     // 클라이언트에서 get 요청이 들어왔을
   (req, res) => {                    // 요청 핸들러: 요청이 들어 왔을 때 호출되는 매서드
     res.set('access-control-allow-origin', '*'); // CORS 문제 해결
     res.set('content-Type', 'text/plain; charset=UTF-8');
-    res.send("Hello!");
+    res.send("Hello! (서영훈)");
   } 
 ); 
+
+// 클라이언트 요청을 다른 서버에게 보낸다.
+app.get('/proxy', (req, res) => {
+  console.log(req.query.url);
+
+  request.get({
+    url: req.query.url
+  }, (error, response, body) => {
+    console.log("네이버에서 응답 받았음!");
+  });
+
+  res.set('access-control-allow-origin', '*');
+  res.set('content-Type', 'text/plain; charset=UTF-8');
+  res.send("Hello! proxy!");
+}); 
 
 // 웹 서버 실행하기
 app.listen(
